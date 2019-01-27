365ae0c3e1696b1395aba4cf2634f20d2ddc6c11
Alishabhoodatt <47074810+Alishabhoodatt@users.noreply.github.com>
2019-01-27 12:50:46 +0530

    Add files via upload

---
+++ ANTI_TRAP_PROJECT.ino
@@ -0,0 +1,90 @@
+#include<SoftwareSerial.h>
+SoftwareSerial sim(10,11);
+int timeout;
+String _buffer;
+String number = "9130101272";
+const int sensor=A1; // Assigning analog pin A1 to variable 'sensor'
+float tempc;  //variable to store temperature in degree Celsius
+float vout;  //temporary variable to hold sensor reading
+
+void setup() {
+  // put your setup code here, to run once:
+delay (7000);
+_buffer.reserve(50);
+int i;
+for(i=6;i<10;i++)
+pinMode(i,INPUT);
+pinMode(A1,INPUT);
+pinMode(12,OUTPUT);
+Serial.begin(9600);
+sim.begin(9600);
+Serial.println("system started...");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (12,LOW);
+delayMicroseconds(2);
+digitalWrite(12,HIGH);
+delayMicroseconds(10);
+digitalWrite (12,LOW);
+
+int d1;
+int d2;
+int t1;
+int t2;
+
+t1=pulseIn(7,HIGH);
+t2=pulseIn(8,HIGH);
+d1=t1*0.0343/2;
+d2=t2*0.0343/2;
+
+int PIRout = digitalRead(9);
+
+int vibration = digitalRead(6);
+
+vout=analogRead(sensor);
+vout=(vout*500)/1023;
+tempc=vout; // Storing value in Degree Celsius
+
+Serial.print("in DegreeC=");
+Serial.print("\t");
+Serial.print(tempc);
+Serial.println();
+delay(1000); //Delay of 1 second for ease of viewing 
+
+if (PIRout==1 && tempc>=40 && d1==d2<=12 && vibration==0 );
+  {Serial.println("warning!,there is someone trapped inside the car");
+  SendMessage();
+  delay(100000);
+  }
+}
+void SendMessage(){
+  ("sending message");
+  sim.println("AT+CMGF=1"); // SETS THE GSM IN TEXT MODE
+  delay(1000);
+  Serial.println("set sms no. ");
+  sim.println("AT+CMGS=\""+number+"\"\r");
+  delay(1000);
+  String SMS ="WARNING!SOMBODY IS TRAPPED INSIDE YOUR CAR NO.4444,SAVE THEM QUICKLY";
+  sim.println(SMS);
+  delay(100);
+  sim.println((char)26);
+  delay(1000);
+  _buffer = _readSerial();
+}
+String _readSerial(){
+  timeout=0;
+  while(!sim.available() && timeout < 12000)
+  {delay(13);
+  timeout++;
+  }
+  if (sim.available()){
+    return sim.readString();
+  }
+}
+
+
+
+
+
---
+++ FUEL_SAVING.ino
@@ -0,0 +1,68 @@
+volatile byte pulses;
+unsigned int rpm;
+unsigned long timeold;
+unsigned int pulsesperturn=4;
+int T3;
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(7,OUTPUT); //relay:
+ pinMode(8,INPUT); //vibration sensor:
+ pinMode(9,INPUT); //Ultrasonic 1:
+ pinMode(10,INPUT); //ultrasonic 2:
+ pinMode(11,INPUT); //rpm:
+ pinMode(12,OUTPUT); //trigger to US 1:
+ pinMode(13,OUTPUT); //trigger to US 2:
+ attachInterrupt(11,counter,FALLING);
+ pulses=0;
+ rpm=0;
+ timeold=0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //vibration sensor:
+  int vib=digitalRead(8);
+
+  //US 1:
+  digitalWrite(12,LOW);
+  delayMicroseconds(2);
+  digitalWrite(12,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(12,LOW);
+  int T1=pulseIn(9,HIGH);
+  int D1=T1*(3.43/2);
+
+  //US 2:
+  digitalWrite(13,LOW);
+  delayMicroseconds(2);
+  digitalWrite(13,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(13,LOW);
+  int T2=pulseIn(10,HIGH);
+  int D2=T2*(3.43/2);
+
+  //rpm:
+  if(millis() - timeold >= 1000){
+    detachInterrupt(11);
+    rpm=(60*1000/pulsesperturn)/(millis()-timeold)*pulses;
+    timeold=millis;
+    pulses=0;
+    attachInterrupt(11,counter,FALLING);
+    if(rpm=0){
+      T3=pulseIn(rpm,LOW);
+    }
+  }
+
+  //conditions:
+  if(vib==HIGH&&(D1<=1||D2<=1)&&T3>20000){
+    digitalWrite(7,HIGH);
+  }
+  else{
+    digitalWrite(7,LOW);
+  }
+}
+
+void counter(){
+  pulses++;
+}
+
